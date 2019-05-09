16578b07a6f741495972f49a5c2679fcc650019d
gz-zhangzicong <zicong.zhang@freemud.com>
2019-05-09 14:59:44 +0800

    去除首页的过滤

---
+++ frontend/uni/pages/index/index.vue
@@ -0,0 +1,21 @@
+<template>
+	<view class="p-index">
+
+    <!-- @fm template start -->
+
+    <Address />
+    <SearchBar />
+    <!-- <Banner /> -->
+    <entryList />
+    <cmCountdownDiscount />
+
+    <!-- @fm template end -->
+
+	</view>
+</template>
+
+<script>
+	export default {
+    name: 'p-index',
+	}
+</script>
